5b694421095f05081ff4f141c54190e3b0e39f2b
Zero-AIRI <186377311+Zero-AIRI@users.noreply.github.com>
2025-04-21 14:55:32 +0800

    网页

--- 网页自制/app.py
+++ 网页自制/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
 #   从flask里拿出两件工具 Flask(用来造网站的主工具)和render_template(用来显示网页的工具)直译为渲染模板
 
-app = Flask(__name__)
+my_wy = Flask(__name__)
+# 对本文件使用Flask类进行处理 并将其命名为my_wy
 
-@app.route("/")
-def home():
+@my_wy.route("/")
+def zhuye():
     return render_template("/bg.html")  
 
-@app.route("/school")
+@my_wy.route("/school")
 def about():
     return render_template("school.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    my_wy.run(debug=True)
